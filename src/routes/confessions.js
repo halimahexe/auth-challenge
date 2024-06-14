254800e98bf1bd7c4ecbd5a9d523810c696e1e31
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-06-14 14:53:18 +0100

    passed challenge 5 - hide confessions

--- src/routes/confessions.js
+++ src/routes/confessions.js
@@ -1,22 +1,21 @@
 const {
-  listConfessions,
-  createConfession,
-} = require("../model/confessions.js");
-const { Layout } = require("../templates.js");
+	listConfessions,
+	createConfession,
+} = require('../model/confessions.js');
+const { Layout } = require('../templates.js');
+const { getSession } = require('../model/session.js');
 
 function get(req, res) {
-  /**
-   * Currently any user can view any other user's private confessions!
-   * We need to ensure only the logged in user can see their page.
-   * [1] Get the session ID from the cookie
-   * [2] Get the session from the DB
-   * [3] Get the logged in user's ID from the session
-   * [4] Get the page owner from the URL params
-   * [5] If the logged in user is not the page owner send a 401 response
-   */
-  const confessions = listConfessions(req.params.user_id);
-  const title = "Your secrets";
-  const content = /*html*/ `
+	const sid = req.signedCookies.sid;
+	const session = getSession(sid);
+	const current_user = session && session.user_id;
+	const page_owner = req.params.user_id;
+	if (current_user !== page_owner) {
+		return res.status(401).send('<h1>You cannot view this page.</h1>');
+	}
+	const confessions = listConfessions(req.params.user_id);
+	const title = 'Your secrets';
+	const content = /*html*/ `
     <div class="Cover">
       <h1>${title}</h1>
       <form method="POST" class="Stack" style="--gap: 0.5rem">
@@ -25,35 +24,35 @@ function get(req, res) {
       </form>
       <ul class="Center Stack">
         ${confessions
-          .map(
-            (entry) => `
+					.map(
+						(entry) => `
             <li>
               <h2>${entry.created_at}</h2>
               <p>${entry.content}</p>
             </li>
             `
-          )
-          .join("")}
+					)
+					.join('')}
       </ul>
     </div>
   `;
-  const body = Layout({ title, content });
-  res.send(body);
+	const body = Layout({ title, content });
+	res.send(body);
 }
 
 function post(req, res) {
-  /**
-   * Currently any user can POST to any other user's confessions (this is bad!)
-   * We can't rely on the URL params. We can only trust the cookie.
-   * [1] Get the session ID from the cookie
-   * [2] Get the session from the DB
-   * [3] Get the logged in user's ID from the session
-   * [4] Use the user ID to create the confession in the DB
-   * [5] Redirect back to the logged in user's confession page
-   */
-  const current_user = Number(req.params.user_id);
-  createConfession(req.body.content, current_user);
-  res.redirect(`/confessions/${current_user}`);
+	/**
+	 * Currently any user can POST to any other user's confessions (this is bad!)
+	 * We can't rely on the URL params. We can only trust the cookie.
+	 * [1] Get the session ID from the cookie
+	 * [2] Get the session from the DB
+	 * [3] Get the logged in user's ID from the session
+	 * [4] Use the user ID to create the confession in the DB
+	 * [5] Redirect back to the logged in user's confession page
+	 */
+	const current_user = Number(req.params.user_id);
+	createConfession(req.body.content, current_user);
+	res.redirect(`/confessions/${current_user}`);
 }
 
 module.exports = { get, post };
