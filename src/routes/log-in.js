18151dfbbc90e1ef47b1bdcc246c848995380367
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-06-14 14:11:26 +0100

    passed challenge 3 - log in

--- src/routes/log-in.js
+++ src/routes/log-in.js
@@ -1,9 +1,11 @@
-const { getUserByEmail } = require("../model/user.js");
-const { Layout } = require("../templates.js");
+const { getUserByEmail } = require('../model/user.js');
+const { Layout } = require('../templates.js');
+const bcrypt = require('bcryptjs');
+const { createSession } = require('../model/session.js');
 
 function get(req, res) {
-  const title = "Log in to your account";
-  const content = /*html*/ `
+	const title = 'Log in to your account';
+	const content = /*html*/ `
     <div class="Cover">
       <h1>${title}</h1>
       <form method="POST" class="Row">
@@ -19,24 +21,39 @@ function get(req, res) {
       </form>
     </div>
   `;
-  const body = Layout({ title, content });
-  res.send(body);
+	const body = Layout({ title, content });
+	res.send(body);
 }
 
 function post(req, res) {
-  const { email, password } = req.body;
-  const user = getUserByEmail(email);
-  if (!email || !password || !user) {
-    return res.status(400).send("<h1>Login failed</h1>");
-  }
-  res.send("to-do");
-  /**
-   * [1] Compare submitted password to stored hash
-   * [2] If no match redirect back to same page so user can retry
-   * [3] If match create a session with their user ID,
-   *     set a cookie with the session ID,
-   *     redirect to the user's confession page (e.g. /confessions/3)
-   */
+	const { email, password } = req.body;
+	const user = getUserByEmail(email);
+	if (!email || !password || !user) {
+		return res.status(400).send('<h1>Login failed</h1>');
+	} else {
+		const storedPass = user.hash;
+		bcrypt.compare(password, storedPass).then((match) => {
+			if (!match) {
+				return res.status(400).send('<h1>Login failed</h1>');
+			} else {
+				const session_id = createSession(user.id);
+				res.cookie('sid', session_id, {
+					signed: true,
+					httpOnly: true,
+					maxAge: 6000,
+					sameSite: 'lax',
+				});
+				res.redirect(`/confessions/${user.id}`);
+			}
+		});
+	}
+	/**
+	 * [1] Compare submitted password to stored hash
+	 * [2] If no match redirect back to same page so user can retry
+	 * [3] If match create a session with their user ID,
+	 *     set a cookie with the session ID,
+	 *     redirect to the user's confession page (e.g. /confessions/3)
+	 */
 }
 
 module.exports = { get, post };
